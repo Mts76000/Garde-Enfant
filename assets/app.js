58bace8b4652fd2de91b81b4f439a15451a7c807
bilel.d <bileldounar@mail.com>
2024-03-12 15:42:15 +0100

    JS burger, link to home

--- assets/app.js
+++ assets/app.js
@@ -2,3 +2,18 @@ import './styles/app.scss';
 import './styles/home.scss';
 
 console.log('ok home');
+
+
+const burgerBtn = document.querySelector('.burger');
+const closeBtn = document.querySelector('.close-burger');
+const burgerMenu = document.querySelector('#burger-menu');
+
+// Ajoutez un gestionnaire d'événements au clic sur le bouton
+burgerBtn.addEventListener('click', (e) => {
+     e.preventDefault();
+     burgerMenu.classList.toggle('active');
+});
+closeBtn.addEventListener('click', (e) => {
+     e.preventDefault();
+     burgerMenu.classList.toggle('active');
+});

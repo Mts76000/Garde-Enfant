879a671117f7d2f8e80766e64a83838050162414
Tristan234 <tristan76190@icloud.com>
2024-03-13 15:39:02 +0100

    user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -14,8 +14,17 @@ class UserController extends AbstractController
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
         ]);
-    } 
-    
+    }
+
+    #[Route('/rdv', name: 'app_rdv')]
+    public function rdv(): Response
+    {
+
+        return $this->render('user/rdv.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
     #[Route('/success', name: 'app_user_success')]
     public function success(): Response
     {

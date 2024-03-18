6fd4ab36833ea6084666c66c0f8f7ac609aa9824
axelclaude <axelclaude@gmail.com>
2024-03-18 10:52:25 +0100

    Document New est fonctionnel

--- src/Controller/DocumentController.php
+++ src/Controller/DocumentController.php
@@ -7,9 +7,12 @@ use App\Form\DocumentType;
 use App\Repository\DocumentRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/document')]
 class DocumentController extends AbstractController
@@ -23,21 +26,50 @@ class DocumentController extends AbstractController
     }
 
     #[Route('/new', name: 'app_document_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, SluggerInterface $slugger, EntityManagerInterface $entityManager): Response
     {
         $document = new Document();
         $form = $this->createForm(DocumentType::class, $document);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($document);
-            $entityManager->flush();
+            /** @var UploadedFile $brochureFile */
+            $brochureFile = $form->get('brochure')->getData();
 
-            return $this->redirectToRoute('app_document_index', [], Response::HTTP_SEE_OTHER);
+            // this condition is needed because the 'brochure' field is not required
+            // so the PDF file must be processed only when a file is uploaded
+            if ($brochureFile) {
+                $originalFilename = pathinfo($brochureFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$brochureFile->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $brochureFile->move(
+                        $this->getParameter('brochures_directory'),
+                        $newFilename,
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $document->setBrochureFilename($newFilename);
+                $entityManager->persist($document);
+                $entityManager->flush();
+
+            } else {
+                $document->setBrochureFilename('default_filename.pdf');
+            }
+
+            // ... persist the $product variable or any other work
+
+            return $this->redirectToRoute('app_document_new');
         }
 
         return $this->render('document/new.html.twig', [
-            'document' => $document,
             'form' => $form,
         ]);
     }

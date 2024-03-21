37298ef5f47e1499d08b063a6fc6223b99128f7e
bilel.d <bileldounar@mail.com>
2024-03-21 14:46:43 +0100

    fixture

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -21,8 +21,7 @@ class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-
-
+        // Création d'un utilisateur avec le rôle "ROLE_ADMIN"
         $admin = new User();
         $admin->setEmail('admin@admin.com');
         $hashedPassword = $this->passwordHasher->hashPassword(
@@ -30,22 +29,45 @@ class UserFixtures extends Fixture
             'admin'
         );
         $admin->setPassword($hashedPassword);
-        $admin->setRoles(array('ROLE_ADMIN'));
+        $admin->setRoles(['ROLE_ADMIN']);
         $admin->setNom('Bertollucci');
-        $admin->setPrenom('michel');
+        $admin->setPrenom('Michel');
         $admin->setZIP(75320);
-        $admin->setAdresse('Rue des rosiers');
-        $manager->persist($admin); 
-    
-       
-        // role
+        $admin->setAdresse('Rue des Rosiers');
+        $manager->persist($admin);
 
-        // parent
+
+        $user = new User();
+        $user->setEmail('user@user.com');
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $user,
+            'password'
+        );
+        $user->setPassword($hashedPassword);
+        $user->setRoles(['ROLE_USER']);
+        $user->setNom('Doe');
+        $user->setPrenom('John');
+        $user->setZIP(12345);
+        $user->setAdresse('123 Main Street');
+        $manager->persist($user);
+
+
+        $pro = new User();
+        $pro->setEmail('pro@pro.com');
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $pro,
+            'password'
+        );
+        $pro->setPassword($hashedPassword);
+        $pro->setRoles(['ROLE_PRO']);
+        $pro->setNom('Smith');
+        $pro->setPrenom('Alice');
+        $pro->setZIP(54321);
+        $pro->setAdresse('456 Elm Street');
+        $manager->persist($pro);
 
         $manager->flush();
 
-
         $this->addReference(self::ADMIN_USER_REFERENCE, $admin);
-
     }
 }

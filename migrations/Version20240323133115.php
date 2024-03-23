81d0739f6c90e0da34f8f4962d879140af898998
axelclaude <axelclaude@gmail.com>
2024-03-23 14:33:40 +0100

    Modification index.html.twig de add_creche

--- migrations/Version20240323133115.php
+++ migrations/Version20240323133115.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240322141129 extends AbstractMigration
+final class Version20240323133115 extends AbstractMigration
 {
     public function getDescription(): string
     {

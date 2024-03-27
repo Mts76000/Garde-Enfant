2ed2c822464d0a15b18b5efa88a9cdac7e8eb34a
mathis-lamotte <lamottemathis@gmail.com>
2024-03-27 11:18:58 +0100

    ajout des rdv pour les user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,23 +7,50 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
+use App\Repository\FullChildRepository;
+use App\Repository\RdvRepository;
 
 
 
 class UserController extends AbstractController
 {
     #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    public function index(RdvRepository $rdvRepository, FullChildRepository $fullchildRepository): Response
     {
         $user = $this->getUser();
         $enfants = $user->getFullChildren();
+
+        $rdvs = $rdvRepository->findBy(['status' => 'open']);
+
+
+        $childs = [];
+        foreach ($rdvs as $rdv) {
+            $childId = $rdv->getIdChild();
+            if ($childId) {
+                $child = $fullchildRepository->find($childId);
+                if ($child) {
+                    $childs[$rdv->getId()] = $child;
+                }
+            } 
+            
+            $pros = [];
+        foreach ($rdvs as $rdv) {
+            $pro = $rdv->getPro();
+            if ($pro) {
+                $pros[$rdv->getId()] = $pro;
+            }
+        }
 
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
             'full_children' => $enfants,
             'user' => $user,
+            'rdvs' => $rdvs,
+            'childs' => $childs,
+            'pros' => $pros,
         ]);
     }
+}
 
     #[Route('/user_success', name: 'app_user_success')]
     public function success(): Response

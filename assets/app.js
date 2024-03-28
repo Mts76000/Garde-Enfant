f4dd95e2cb570692218ba6ef273b10deb481d499
axelclaude <axelclaude@gmail.com>
2024-03-28 15:27:13 +0100

    Mise à jour de stripe

--- assets/app.js
+++ assets/app.js
@@ -26,9 +26,7 @@ import "./styles/user/index.scss";
 import "./styles/user/addchild.scss";
 import "./styles/user/recupChild.scss";
 import "./styles/user/listing-child.scss";
-
-
-
+import "./styles/user/stripe.scss";
 
 
 console.log("ok home");
@@ -39,31 +37,31 @@ const burgerMenu = document.querySelector("#burger-menu");
 
 // Ajoutez un gestionnaire d'événements au clic sur le bouton
 burgerBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  burgerMenu.classList.toggle("active");
+    e.preventDefault();
+    burgerMenu.classList.toggle("active");
 });
 closeBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  burgerMenu.classList.toggle("active");
+    e.preventDefault();
+    burgerMenu.classList.toggle("active");
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  var btnTop = document.getElementById("btnTop");
+    var btnTop = document.getElementById("btnTop");
 
-  window.addEventListener("scroll", function () {
-    if (window.pageYOffset > 100) {
-      btnTop.style.display = "block";
-    } else {
-      btnTop.style.display = "none";
-    }
-  });
-
-  btnTop.addEventListener("click", function () {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
+    window.addEventListener("scroll", function () {
+        if (window.pageYOffset > 100) {
+            btnTop.style.display = "block";
+        } else {
+            btnTop.style.display = "none";
+        }
     });
-  });
+
+    btnTop.addEventListener("click", function () {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    });
 });
 
 
@@ -72,17 +70,45 @@ var map = L.map('map').setView([0, 0], 16); // Initialiser la carte avec une vue
 L.tileLayer(`https://api.maptiler.com/maps/basic-v2-dark/{z}/{x}/{y}.png?key=${key}`).addTo(map); // Fond de carte OpenStreetMap
 
 fetch(`https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(address)}&format=json`)
-  .then(response => response.json())
-  .then(data => {
-    var latitude = parseFloat(data[0].lat);
-    var longitude = parseFloat(data[0].lon);
+    .then(response => response.json())
+    .then(data => {
+        var latitude = parseFloat(data[0].lat);
+        var longitude = parseFloat(data[0].lon);
 
-    map.setView([latitude, longitude], 16);
+        map.setView([latitude, longitude], 16);
 
-    var marker = L.marker([latitude, longitude]).addTo(map);
-    var popupContent = `<div style="text-align: center;"><h3 style="color: #344D67;"><b>${nomAdresse}</h3></b><br> ${address}</div>`;
+        var marker = L.marker([latitude, longitude]).addTo(map);
+        var popupContent = `<div style="text-align: center;"><h3 style="color: #344D67;"><b>${nomAdresse}</h3></b><br> ${address}</div>`;
 
-    marker.bindPopup(popupContent).openPopup();
-  })
-  .catch(error => console.error('Erreur lors du géocodage:', error));
+        marker.bindPopup(popupContent).openPopup();
+    })
+    .catch(error => console.error('Erreur lors du géocodage:', error));
 
+// Gestion de Stripe
+
+src="https://js.stripe.com/v3/";
+
+var stripe = Stripe('pk_test_51OyxKeP2itdtN1tGWeSug7qNNbEf60ZpH2g0trZPPj7FJ7N4tBTT8GBFH3oIiZCgCDlfi0VdAaAYRlnpjNf7nWU400vG6dIzhB');
+var elements = stripe.elements();
+var cardElement = elements.create('card');
+cardElement.mount('#card-element');
+
+var form = document.getElementById('payment-form');
+var cardButton = document.getElementById('card-button');
+var clientSecret = cardButton.getAttribute('data-secret');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    stripe.handleCardPayment(clientSecret, cardElement, {
+        payment_method_data: {
+            billing_details: {name: document.getElementById('cardholder-name').value}
+        }
+    }).then(function (result) {
+        if (result.error) {
+            var errorElement = document.getElementById('error-message');
+            errorElement.textContent = result.error.message;
+        } else {
+            window.location.href = 'confirmation.html';
+        }
+    });
+});
--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -10,25 +10,6 @@ use Stripe;
 
 class PaymentController extends AbstractController
 {
-    #[Route('/order/create-session-stripe', name: 'app_payment')]
-    public function stripeCheckout(): RedirectResponse
-    {
-        \Stripe\Stripe::setApiKey('sk_test_VePHdqKTYQjKNInc7u56JBrQ');
-        header('Content-Type: application/json');
-
-        $YOUR_DOMAIN = 'http://localhost:4242';
-
-        $checkout_session = \Stripe\Checkout\Session::create([
-            'line_items' => [[
-                # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
-                'price' => '{{PRICE_ID}}',
-                'quantity' => 1,
-            ]],
-            'mode' => 'payment',
-            'success_url' => $YOUR_DOMAIN . '/success.html',
-            'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
-        ]);
-    }
 
     #[Route('/payment', name: 'app_payment')]
     public function index(): Response

7f739c57856e7532f0805b70069590166fd4b3c3
bilel.d <bileldounar@mail.com>
2024-03-28 11:49:53 +0100

    Update ContactCrecheController to use AddCrecheRepository; modify database schema of pro\_time table
    The ContactCrecheController has been updated to use the AddCrecheRepository to retrieve the creche ID associated with the current user. The index action has been modified to display only the contact creches associated with the user's creche.
    
    Additionally, the database schema of the pro\_time table has been modified to change the data type of the heure\_debut, heure\_fin, and jour columns, and to add a foreign key constraint to the pro\_id column. A subsequent migration has been added to revert these changes.

--- src/Controller/ContactCrecheController.php
+++ src/Controller/ContactCrecheController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\ContactCreche;
 use App\Form\ContactCrecheType;
+use App\Repository\AddCrecheRepository;
 use App\Repository\ContactCrecheRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,44 +16,62 @@ use Symfony\Component\Routing\Attribute\Route;
 class ContactCrecheController extends AbstractController
 {
     #[Route('/', name: 'app_contact_creche_index', methods: ['GET'])]
-    public function index(ContactCrecheRepository $contactCrecheRepository): Response
+    public function index(ContactCrecheRepository $contactCrecheRepository, AddCrecheRepository $addCrecheRepository): Response
     {
+        $user = $this->getUser();
+        $crecheId = $addCrecheRepository->findCrecheIdByUserIdSingle($user);
+
+
         return $this->render('contact_creche/index.html.twig', [
-            'contact_creches' => $contactCrecheRepository->findAll(),
+            'contact_creches' => $contactCrecheRepository->findby(['id_pro' => $crecheId]),
+            'user' => $user,
         ]);
     }
 
     #[Route('/new', name: 'app_contact_creche_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $user = $this->getUser();
+
+        $idcreche = $request->query->get('id');
+
         $contactCreche = new ContactCreche();
         $form = $this->createForm(ContactCrecheType::class, $contactCreche);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            $contactCreche->setIdPro($idcreche);
             $entityManager->persist($contactCreche);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_contact_creche_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_rdv_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('contact_creche/new.html.twig', [
             'contact_creche' => $contactCreche,
             'form' => $form,
+            'user' => $user,
         ]);
     }
 
     #[Route('/{id}', name: 'app_contact_creche_show', methods: ['GET'])]
     public function show(ContactCreche $contactCreche): Response
     {
+        $user = $this->getUser();
+
         return $this->render('contact_creche/show.html.twig', [
             'contact_creche' => $contactCreche,
+            'user' => $user,
+
         ]);
     }
 
     #[Route('/{id}/edit', name: 'app_contact_creche_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, ContactCreche $contactCreche, EntityManagerInterface $entityManager): Response
     {
+        $user = $this->getUser();
+
         $form = $this->createForm(ContactCrecheType::class, $contactCreche);
         $form->handleRequest($request);
 
@@ -65,17 +84,23 @@ class ContactCrecheController extends AbstractController
         return $this->render('contact_creche/edit.html.twig', [
             'contact_creche' => $contactCreche,
             'form' => $form,
+            'user' => $user,
+
         ]);
     }
 
     #[Route('/{id}', name: 'app_contact_creche_delete', methods: ['POST'])]
     public function delete(Request $request, ContactCreche $contactCreche, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$contactCreche->getId(), $request->request->get('_token'))) {
+        $user = $this->getUser();
+
+        if ($this->isCsrfTokenValid('delete' . $contactCreche->getId(), $request->request->get('_token'))) {
             $entityManager->remove($contactCreche);
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_contact_creche_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_contact_creche_index', [
+            'user' => $user,
+        ], Response::HTTP_SEE_OTHER);
     }
 }
